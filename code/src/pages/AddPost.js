7c06ec7158e774414a948e54bc3e072e1bac279d
Py0000 <pylim8266@gmail.com>
2023-10-07 00:48:33 +0800

    Add more user friendly error handling

--- code/src/pages/AddPost.js
+++ code/src/pages/AddPost.js
@@ -10,6 +10,68 @@ function AddPost(isAuth) {
     const [author, setAuthor] = useState("");
     const [rating, setRating] = useState("");
     const [review, setReview] = useState("");
+    const [formErrors, setFormErrors] = useState({
+        title: "",
+        author: "",
+        rating: "",
+        review: ""
+    });
+
+    const isValidTitle = (titleValue) => {
+        if (titleValue.length <= 0) {
+            return "Title should not be empty.";
+        }
+        return "";
+    };
+
+    const isValidRating = (value) => {
+        const ratingValue = parseFloat(value);
+        if (isNaN(ratingValue) || ratingValue < 0 || ratingValue > 5) {
+            return "Please enter a valid rating between 0 and 5.";
+        }
+        return "";
+    };
+
+    const isValidReview = (reviewValue) => {
+        if (reviewValue.length <= 0) {
+            return "Review should not be empty.";
+        }
+        return "";
+    };
+
+    const handleTitleChange = (e) => {
+        const value = e.target.value;
+
+        setFormErrors(errors => ({
+            ...errors,
+            title: isValidTitle(value)
+        }));
+
+        setTitle(value);
+    };
+    
+    const handleRatingChange = (e) => {
+        const value = e.target.value;
+
+        setFormErrors(errors => ({
+            ...errors,
+            rating: isValidRating(value)
+        }));
+
+        setRating(value);
+    };
+
+    const handleReviewChange = (e) => {
+        const value = e.target.value;
+
+        setFormErrors(errors => ({
+            ...errors,
+            review: isValidReview(value)
+        }));
+
+        setReview(value);
+    };
+
 
     // Guard function (additional security measure)
     // Redirect user to login page if not authenticated
@@ -40,6 +102,7 @@ function AddPost(isAuth) {
             dateCreated: currentDate
         }
 
+
         // Arg 1: reference to the database collection (i.e. "table") you want to the info add to
         // Arg 2: Data that should be added
         await addDoc(reviewsCollectionRef, data);
@@ -49,21 +112,22 @@ function AddPost(isAuth) {
     return (  
         <div className="add-post-page">
             <h1>Create a Review!</h1>
+            {formErrors.title && <p className="error-text">{formErrors.title}</p> || formErrors.rating && <p className="error-text">{formErrors.rating}</p> || formErrors.review && <p className="error-text">{formErrors.review}</p>}  
             <div className="input-field">
                 <label>Book Title: </label>
-                <input placeholder="Title" onChange={(e) => setTitle(e.target.value)}></input>
+                <input placeholder="Title" onChange={handleTitleChange} className={formErrors.title ? "error-input" : ""}></input>
             </div>
             <div className="input-field">
                 <label>Author: </label>
-                <input placeholder="Author" onChange={(e) => setAuthor(e.target.value)}></input>
+                <input placeholder="Author" onChange={(e) => setAuthor(e.target.value)} ></input>
             </div>
             <div className="input-field">
                 <label>Rating: </label>
-                <input placeholder="out of 5" onChange={(e) => setRating(e.target.value)}></input>
+                <input placeholder="out of 5" onChange={handleRatingChange} className={formErrors.rating ? "error-input" : ""}></input>
             </div>
             <div className="input-field">
                 <label>Review: </label>
-                <textarea placeholder="Write your thoughts here..." onChange={(e) => setReview(e.target.value)}></textarea>
+                <textarea placeholder="Write your thoughts here..." onChange={handleReviewChange} className={formErrors.review ? "error-input" : ""}></textarea>
             </div>
             <button onClick={createReview}> Submit My Review</button>
         </div>
